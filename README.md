e27cad617ad6f061e315c7f8f9785f079a9430e8
Mason <150401281+SunnyMayo@users.noreply.github.com>
2024-11-21 15:43:43 +1100

    Updated js functions and css styling
    Added mouseover functionality to each gridbox element
    Added opacity feature to increment by 10% each mouseover event
    Added reset button
    Added grid size prompt

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # etch-a-sketch
 Simple etch-a-sketch project as part of The Odin Project
+
+Page initialises with a 16x16 pixel grid that responds to mouseover by darkening the square.
+
+Users can reset the grid to 16x16 at any time, or enter their own number between 1-100 to set the grid to that length and width.
+
+Each square increases in opacity every time the mouse hovers over, up to ten interactions before being fully opaque.
---
+++ script.js
@@ -0,0 +1,50 @@
+const sizeBtn = document.querySelector("#size-btn");
+const resetBtn = document.querySelector("#reset-btn");
+const container = document.querySelector("#container");
+
+sizeBtn.addEventListener("click", () => {
+    let size = parseInt(prompt("Grid length?", 16));
+    if (isNaN(size) || size < 0 || size > 100) {
+        alert("Invalid, please choose a number between 1 and 100.");
+        return;
+    }
+    else {
+        reset();
+        createGrid(size);
+    };
+});
+
+resetBtn.addEventListener("click", reset);
+      
+// reset to empty grid
+function reset() {
+    document.getElementById("container").replaceChildren();
+};
+
+// creates 16x16 div elements within the container
+function createGrid(number) {
+    let gridsize = number * number;
+    for (let i = 0; i < gridsize; i++) {  
+        // create new div assigned to variable "gridbox"
+        const gridbox = document.createElement("div");
+        // add class "gridbox" to the div element
+        gridbox.classList.add("gridbox");
+        // size gridbox to fit 960x960px grid
+        let x = parseFloat(960 / number);
+        gridbox.style.height = `${x}px`;
+        gridbox.style.width = `${x}px`;
+        // add mouseover event that changes background to grey
+        let opacity = 0;
+        gridbox.addEventListener("mouseover", () => {
+            if (opacity < 1.0) {
+                opacity += 0.1;
+                gridbox.style.opacity = opacity;
+            }
+        });
+        // appends each gridbox to the container
+        container.appendChild(gridbox);
+    };
+};
+
+// create initial 16x16 grid
+createGrid(16);
